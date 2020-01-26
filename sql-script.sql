3e4422969d963220db3874dfb3b9f4ce482b65ef
eemektas <eemektas@gmail.com>
2020-01-27 00:24:03 +0300

    still loading

--- sql-script.sql
+++ sql-script.sql
@@ -4,7 +4,7 @@ USE products_db;
 
 CREATE TABLE IF NOT EXISTS products_table (
 product_id INT AUTO_INCREMENT PRIMARY KEY
-#product_name VARCHAR(255),
-#product_image VARCHAR(255),
-#product_price VARCHAR(50)
+product_name VARCHAR(255),
+product_image VARCHAR(255),
+product_price VARCHAR(50)
 );
